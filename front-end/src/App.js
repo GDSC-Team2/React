72f28d520c4be561b6b3ef2bbf0332ec78a8a627
KIM MINJI <sml07113@gmail.com>
2021-12-21 01:09:26 +0900

    fix: resolve conflicts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import ItemList from './component/ItemList';
-import MyPage from './component/MyPage';
-import NavBar from './component/NavBar';
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ItemList />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+
+    </div>
   );
 }
 
