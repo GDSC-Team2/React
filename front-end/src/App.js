b69168efec841e8ab07d69293aec38da24006eab
KIM MINJI <sml07113@gmail.com>
2021-12-21 01:11:34 +0900

    fix: resolve conflicts(2)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import React from 'react';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BtnLogin from './components/BtnLogin';
+
 
 function App() {
   return (
     <div className="App">
-
+      <h6>공구리숙트</h6>
+      <BtnLogin/>
     </div>
   );
 }
