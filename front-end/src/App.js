79151b338ab05ef927ff6d12803573847d0ec154
KIM MINJI <sml07113@gmail.com>
2021-12-20 16:40:56 +0900

    feat: home, mypage 화면 구성

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ItemList from './component/ItemList';
+import MyPage from './component/MyPage';
+import NavBar from './component/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h5>hello world</h5>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemList />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
