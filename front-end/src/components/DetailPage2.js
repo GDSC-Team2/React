d8f80758aa6449db5f1479147716d64e917c2ad1
jiae yoon <68904755+jiaeYoon@users.noreply.github.com>
2021-12-24 03:45:41 +0900

    feat : implement all modify function except date

--- front-end/src/components/DetailPage2.js
+++ front-end/src/components/DetailPage2.js
@@ -1,5 +1,5 @@
-import '../components/Post.css';
-import { useState, useEffect } from "react";
+import '../Post.css';
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
@@ -9,11 +9,6 @@ function DetailPage2(){
 	const [isModifyMode, setModifyMode] = useState(true);
 	const [isWriter, setWriter] = useState(true);
 
-	function ChangeMode() {
-		if(isModifyMode === true) {
-			setModifyMode(false)
-		}
-	}
 
 	const gotoMain = () => {
     document.location.href = '/';
@@ -23,7 +18,7 @@ function DetailPage2(){
 
 	const [posts, setPosts] = useState([]);
   useEffect(() => {
-    axios.get(`/api/v1/posts/`)
+    axios.get("/api/v1/posts/")
       .then(function(response) {
         setPosts(response.data);
       })
@@ -34,19 +29,80 @@ function DetailPage2(){
 
   const postList = posts.filter(post => (post.id === Number(id)));
 
-	// 글 삭제
-  const deleteHandler = (id, e) => {
-    e.preventDefault();
-    if (window.confirm("삭제하시겠습니까?")) {
-      axios.delete(`/api/v1/posts/${id}`)
-      .then(response => {
-        alert("삭제되었습니다.");
-        window.location.href = "/";
-      }).catch(error => {
-        alert(error.response.data);
-      });
-    } 
+	function ChangeMode() {
+		if(isModifyMode === true) {
+			setModifyMode(false)
+			setTitle(postList[0].title);
+			setPrice(postList[0].price);
+			//setDate(postList[0].date);
+			setLink(postList[0].link);
+			setContact(postList[0].contact);
+			setDescription(postList[0].description);
+		}
+	}
+
+	//수정
+	function modifyDone(){
+		axios.put(`/api/v1/posts/${id}`,{
+			...postList,
+			title : "title",
+			author : "react",
+		});
+	}
+	
+	const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [date, setDate] = useState("");
+  const [link, setLink] = useState("");
+  const [contact, setContact] = useState("");
+  const [description, setDescription] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
   }
+
+  const priceChangeHandler = (e) => {
+    setPrice(e.target.value);
+  }
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  }
+
+  const linkChangeHandler = (e) => {
+    setLink(e.target.value);
+  }
+
+  const contactChangeHandler = (e) => {
+    setContact(e.target.value);
+  }
+
+  const descriptionChangeHandler = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.put(`/api/v1/posts/${id}`, 
+    {
+      title: title,
+      price: price,
+      date: date,
+      link: link,
+      contact: contact,
+      description: description
+    }
+    ).then(response => {
+      alert("수정되었습니다.");
+      window.location.href = "/";
+    }).catch(error => {
+      alert(error.response.data);
+    });
+  }
+
+	const setting = (id) => {
+		setTitle((postList) => new Map(postList).set(title))
+	}
 
 	return(
 		<div className='WholeBox'>
@@ -55,70 +111,72 @@ function DetailPage2(){
 					<div className='ContentBox'>
 						<Form className='items'>
 						{postList.map(post => (
-							<>
-								<h2>{post.title}</h2><hr/>
-								<table>
-									<tr>
-										<td>예상가격</td> 
-										<td id="price">{post.price}</td>
-									</tr>
-									<tr> 
-										<td className="item_name">공구기간</td> 
-										<td>{post.date}</td>
-									</tr>
-									<tr> 
-										<td>상품링크</td> 
-										<td>{post.link}</td>
-									</tr>
-									<tr> 
-										<td>오픈채팅</td> 
-										<td>{post.contact}</td>
-									</tr>
-									<tr> 
-										<td>설명</td> 
-										<td>{post.description}</td>
-									</tr>
-								</table>
-								<div className='btnArea1'>
-								<Button variant="primary" onClick={ChangeMode}>수정</Button>
-								<Button variant="danger" onClick={(e) => {deleteHandler(post.id, e)}}>삭제</Button>
-								</div>
-							</>
+							<table>
+								<tr>
+									<td>예상가격</td> 
+									<td id="price">{post.price}</td>
+								</tr>
+								<tr> 
+									<td className="item_name">공구기간</td> 
+									<td>{post.date}</td>
+								</tr>
+								<tr> 
+									<td>상품링크</td> 
+									<td>{post.link}</td>
+								</tr>
+								<tr> 
+									<td>오픈채팅</td> 
+									<td>{post.contact}</td>
+								</tr>
+								<tr> 
+									<td>설명</td> 
+									<td>{post.description}</td>
+								</tr>
+							</table>
 							))}
 						</Form>
+						<div className='btnArea1'>
+							<Button variant="primary" onClick={ChangeMode}>수정</Button>
+							<Button variant="danger">삭제</Button>
+						</div>
 					</div>
 				) : (
 					<div className='ContentBox'>
 						<Form className='items'>
 						{postList.map(post => (
 							<table className='ModifyMode'>
-								<tr>
-									<td>예상가격</td> 
-									<td><Form.Control type="text" value={post.price}/></td>
-								</tr>
 								<tr> 
-									<td>공구기간</td> 
-									<td><Form.Control type="date" value={post.date}/> ~ <Form.Control type="date" /></td>
-								</tr>
-								<tr> 
-									<td>상품링크</td> 
-									<td><Form.Control type="url" value={post.link}/></td>
-								</tr>
-								<tr> 
-									<td>오픈채팅</td> 
-									<td><Form.Control type="url" value={post.contact} /></td>
-								</tr>
-								<tr> 
-									<td>설명</td> 
-									<td><Form.Control type="textarea" value={post.description} /></td>
-								</tr>
+              <td>제목</td> 
+              <td><Form.Control type="text" onChange={titleChangeHandler} value={title}/></td>
+            </tr> 
+            <tr>
+              <td>예상가격</td> 
+              <td><Form.Control type="text" placeholder="12,000" onChange={priceChangeHandler} value={price}/></td>
+            </tr>
+            <tr> 
+              <td>공구기간</td> 
+              {/* <Form.Control type="date"/> ~  */}
+              {/* <td><Form.Control type="date" ref={dateRef}/></td> */}
+              <td><Form.Control type="date" onChange={dateChangeHandler} value={date}/></td>
+            </tr>
+            <tr> 
+              <td>상품링크</td> 
+              <td><Form.Control type="url" onChange={linkChangeHandler} value={link}/></td>
+            </tr>
+            <tr> 
+              <td>오픈채팅</td> 
+              <td><Form.Control type="url" onChange={contactChangeHandler} value={contact}/></td>
+            </tr>
+            <tr> 
+              <td>설명</td> 
+              <td><Form.Control type="textarea" onChange={descriptionChangeHandler} value={description}/></td>
+            </tr>
 							</table>
 						))}
 						</Form>
-						{/* <Button variant="secondary" onClick={navigate('/')}>취소</Button> */}
 						<div className='btnArea2'>
 							<Button variant="secondary" onClick={gotoMain}>취소</Button>
-							<Button variant="primary">완료</Button>
+							<Button variant="primary" onClick={submitHandler}>완료</Button>
 						</div>
 					</div>
 				)
