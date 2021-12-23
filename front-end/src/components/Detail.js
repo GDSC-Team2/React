be45c0a64f632102040bf5e9dc65a327706b5373
KIM MINJI <sml07113@gmail.com>
2021-12-23 22:46:04 +0900

    test: Delete

--- front-end/src/components/Detail.js
+++ front-end/src/components/Detail.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 export default function Detail() {
@@ -18,19 +19,33 @@ export default function Detail() {
 
   const postList = posts.filter(post => (post.id === Number(id))); // 전달받은 id값과 일치하는 데이터
 
+  const deleteHandler = (id) => {
+    // axios.delete(`/api/v1/posts/${id}`)
+    // .then(response => {
+    //   alert("삭제되었습니다.");
+    //   window.location.href = "/";
+    // }).catch(error => {
+    //   alert(error.response.data);
+    // });
+  }
+
   return (
 
   <div>
     {postList.map(post => (
-      <ul key={post.id}>
-        <li>글 제목: {post.title}</li>
-        <li>작성자: {post.author}</li>
-        <li>예상 가격: {post.price}</li>
-        <li>공구 기간: {post.date}</li>
-        <li>상품 링크: {post.link}</li>
-        <li>오픈채팅: {post.contact}</li>
-        <li>설명: {post.description}</li>
-      </ul>
+      <div>
+        <ul key={post.id}>
+          <li>글 제목: {post.title}</li>
+          <li>작성자: {post.author}</li>
+          <li>예상 가격: {post.price}</li>
+          <li>공구 기간: {post.date}</li>
+          <li>상품 링크: {post.link}</li>
+          <li>오픈채팅: {post.contact}</li>
+          <li>설명: {post.description}</li>
+        </ul>
+
+        <Button variant="danger" onClick={deleteHandler(post.id)}>삭제</Button>
+      </div>
     ))}
   </div>
   );
