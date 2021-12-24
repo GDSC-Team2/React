b6c5571bec2018f15d665dc3c5492cdd8831453c
jiae yoon <68904755+jiaeYoon@users.noreply.github.com>
2021-12-24 20:17:14 +0900

    feat : check isWriter
    if(is Writer) -> show delete, modify button

--- front-end/src/components/DetailPage2.js
+++ front-end/src/components/DetailPage2.js
@@ -1,5 +1,5 @@
 import '../components/Post.css';
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { useParams } from 'react-router-dom';
 function DetailPage2(){
 
 	const [isModifyMode, setModifyMode] = useState(true);
-	const [isWriter, setWriter] = useState(true);
+	const [isWriter, setWriter] = useState(false);
 
 
 	const gotoMain = () => {
@@ -29,19 +29,31 @@ function DetailPage2(){
 
   const postList = posts.filter(post => (post.id === Number(id)));
 
-		// 글 삭제
-		const deleteHandler = (id, e) => {
-			e.preventDefault();
-			if (window.confirm("삭제하시겠습니까?")) {
-				axios.delete(`/api/v1/posts/${id}`)
-				.then(response => {
-					alert("삭제되었습니다.");
-					window.location.href = "/";
-				}).catch(error => {
-					alert(error.response.data);
-				});
-			} 
+	// 글 작성자인지 확인
+	useEffect(() => {
+		const author = localStorage.getItem("name");
+		const list = postList[0];
+		if(list){
+			const post_author = list.author;
+			if(author === post_author){
+				setWriter(true);
+			}
 		}
+	})
+
+	// 글 삭제
+	const deleteHandler = (id, e) => {
+		e.preventDefault();
+		if (window.confirm("삭제하시겠습니까?")) {
+			axios.delete(`/api/v1/posts/${id}`)
+			.then(response => {
+				alert("삭제되었습니다.");
+				window.location.href = "/";
+			}).catch(error => {
+				alert(error.response.data);
+			});
+		} 
+	}
 
 	function ChangeMode() {
 		if(isModifyMode === true) {
@@ -55,15 +67,7 @@ function DetailPage2(){
 		}
 	}
 
-	//수정
-	function modifyDone(){
-		axios.put(`/api/v1/posts/${id}`,{
-			...postList,
-			title : "title",
-			author : "react",
-		});
-	}
-	
+	//수정	
 	const [title, setTitle] = useState("");
   const [price, setPrice] = useState("");
   const [date, setDate] = useState("");
@@ -114,26 +118,22 @@ function DetailPage2(){
     });
   }
 
-	const setting = (id) => {
-		setTitle((postList) => new Map(postList).set(title))
-	}
-
 	return(
 		<div className='WholeBox'>
 			{isWriter ? (
 				isModifyMode ? (
 					<div className='ContentBox'>
-						<Form className='items'>
 						{postList.map(post => (
+						<Form className='items' key={post.id}>
 							<>
 								<h2>{post.title}</h2><hr/>
 								<table>
 									<tr>
 										<td>예상가격</td> 
-										<td id="price">{post.price}</td>
+										<td>{post.price}</td>
 									</tr>
 									<tr> 
-										<td className="item_name">공구기간</td> 
+										<td>공구기간</td> 
 										<td>{post.date}</td>
 									</tr>
 									<tr> 
@@ -154,15 +154,13 @@ function DetailPage2(){
 									<Button variant="danger" onClick={(e) => {deleteHandler(post.id, e)}}>삭제</Button>
 								</div>
 							</>
-						))}
 						</Form>
-						
+						))}
 					</div>
 				) : (
 					<div className='ContentBox'>
 						<Form className='items'>
-						{postList.map(post => (
-							<table className='ModifyMode'>
+						<table className='ModifyMode'>
 								<tr> 
               <td>제목</td> 
               <td><Form.Control type="text" onChange={titleChangeHandler} value={title}/></td>
@@ -188,7 +186,6 @@ function DetailPage2(){
               <td><Form.Control type="textarea" onChange={descriptionChangeHandler} value={description}/></td>
             </tr>
 							</table>
-						))}
 						</Form>
 						<div className='btnArea2'>
 							<Button variant="secondary" onClick={gotoMain}>취소</Button>
@@ -196,8 +193,39 @@ function DetailPage2(){
 						</div>
 					</div>
 				)
-			):(<></>)
-			}
+			):(
+				<div className='ContentBox'>
+				{postList.map(post => (
+				<Form className='items' key={post.id}>
+					<>
+						<h2>{post.title}</h2><hr/>
+						<table>
+							<tr>
+								<td>예상가격</td> 
+								<td>{post.price}</td>
+							</tr>
+							<tr> 
+								<td>공구기간</td> 
+								<td>{post.date}</td>
+							</tr>
+							<tr> 
+								<td>상품링크</td> 
+								<td>{post.link}</td>
+							</tr>
+							<tr> 
+								<td>오픈채팅</td> 
+								<td>{post.contact}</td>
+							</tr>
+							<tr> 
+								<td>설명</td> 
+								<td>{post.description}</td>
+							</tr>
+						</table>
+					</>
+			</Form>
+			))}
+			</div>
+		)}
 		</div>
   );
 }
